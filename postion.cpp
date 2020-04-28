bb8d284c9a53d89ec9ecf21eab80ad77317ef002
Say-Hello2y <2510193771@qq.com>
2020-04-28 14:34:47 +0800

    采用c语言编写

--- postion.cpp
+++ postion.cpp
@@ -26,10 +26,10 @@ double lat_s=45;}
 //将维度转化为弧度
 double rad(struct lat l1)
 {return l1.lat_d*pi/180+l1.lat_m*pi/180/60+l1.lat_s*pi/180/3600;}
-//将精度装华为维度
+//将精度转化为维度
 double dms2m(struct lng ln)
 {return ln.lng_d*60+ln.lng_m+ln.lng_s/60;}
-//将维度装华为精度
+//将维度转化为精度
 double dms2m2(struct lat ll)
 {return ll.lat_d*60+ll.lat_m+ll.lat_s/60;}
 //位置函数输出方位交rad（弧度）和距离
@@ -54,7 +54,7 @@ S=abs((dms2m2(lat2)-dms2m2(lat1))/cos(C));
 printf("distance:%f海里\n",S);
 return 0;
 }
-/*************求取航迹偏差*************************///////////////////////////////
+/*************求取航迹偏差（计算的是第三个输入地点至第一二个输入地点的航迹偏差）*************************///////////////////////////////
 double geterror(struct lat lat1,struct lng lng1,struct lat lat2,struct lng lng2,struct lat lat3,struct lng lng3)
 {
 double radlat1=rad(lat1);
@@ -93,11 +93,11 @@ return 0;
 //主函数
 void main()
 {struct lat lat_dalian={38,54,45};//大连维度
-struct lat lat_shanghai={31,11,0};//上海
-struct lng lng_dalian={121,36,9};//大连
-struct lng lng_shanghai={121,29,0};//上海
-struct lat lat_yantai={37,52,0};//烟台
-struct lng lng_yantai={121,39,0};//烟台
+struct lat lat_shanghai={31,11,0};//上海纬度
+struct lng lng_dalian={121,36,9};//大连精度
+struct lng lng_shanghai={121,29,0};//上海经度
+struct lat lat_yantai={37,52,0};//烟台纬度
+struct lng lng_yantai={121,39,0};//烟台经度
 printf("dalian to shanghai:\n");
 getpostion(lat_dalian,lng_dalian,lat_shanghai,lng_shanghai);
 printf("dalian to yantai:\n");
